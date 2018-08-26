58fd1385c6cdd7f914edec39a6025f6a80261c91
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-08-26 19:30:41 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/lph/adminp2/AdminP2Application.java
@@ -0,0 +1,12 @@
+package br.com.lph.adminp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminP2Application.class, args);
+	}
+}
