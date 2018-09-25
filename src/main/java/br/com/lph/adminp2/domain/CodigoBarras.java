c889ae662a63c43aa1949fbef5952636f8c93fba
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-09-25 12:23:32 -0300

    Correcao referencia ciclica (@JsonManagedReference/@JsonBackReference) @JsonIgnore

--- src/main/java/br/com/lph/adminp2/domain/CodigoBarras.java
+++ src/main/java/br/com/lph/adminp2/domain/CodigoBarras.java
@@ -9,9 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class CodigoBarras implements Serializable{
