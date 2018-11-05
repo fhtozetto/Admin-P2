f330f6764aa3c7381eeb94f27209cb62de5a2882
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-11-05 10:24:23 -0300

    Listando todas as categorias com DTO

--- src/main/java/br/com/lph/adminp2/service/CategoriaService.java
+++ src/main/java/br/com/lph/adminp2/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.lph.adminp2.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
 		}	
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
